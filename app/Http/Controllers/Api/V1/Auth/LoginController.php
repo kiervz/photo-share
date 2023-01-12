026c905b6bdda8310405aa390657ac5dffd236d9
Kiervey Maghanoy <kierveymaghanoy24@gmail.com>
2023-01-12 23:37:23 +0800

    add login api route and login controller

---
+++ app/Http/Controllers/Api/V1/Auth/LoginController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LoginController extends Controller
+{
+    public function login(Request $request)
+    {
+        dd("login");
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Api\V1\Auth\LoginController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -13,7 +15,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'v1'], function() {
+    Route::group(['prefix' => 'auth'], function() {
+        Route::post('login', [LoginController::class, 'login'])->name('auth.login');
+    });
 });

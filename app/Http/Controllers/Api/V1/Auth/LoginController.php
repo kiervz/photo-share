1deba2c02bdf7bd88a1237aede5370d423219ed7
Kiervey Maghanoy <kierveymaghanoy24@gmail.com>
2023-01-12 23:40:10 +0800

    add login request

--- app/Http/Controllers/Api/V1/Auth/LoginController.php
+++ app/Http/Controllers/Api/V1/Auth/LoginController.php
@@ -4,10 +4,11 @@ namespace App\Http\Controllers\Api\V1\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\Auth\LoginRequest;
 
 class LoginController extends Controller
 {
-    public function login(Request $request)
+    public function login(LoginRequest $request)
     {
         dd("login");
     }

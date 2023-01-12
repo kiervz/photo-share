ccf7263b0e7da0bb0ecd9f11b2842671c918101a
Kiervey Maghanoy <kierveymaghanoy24@gmail.com>
2023-01-13 01:56:08 +0800

    done fetch all posts

--- app/Http/Controllers/Api/V1/PostController.php
+++ app/Http/Controllers/Api/V1/PostController.php
@@ -4,8 +4,16 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Resources\Post\PostCollection;
+
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    
+    public function index()
+    {
+        $posts = Post::paginate(10);
+
+        return $this->customResponse('Successfully fetched!', new PostCollection($posts));
+    }
 }

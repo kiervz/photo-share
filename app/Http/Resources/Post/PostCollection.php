6003b13645c1e64d297c2c50a2e6d9aff6aa45ff
Kiervey Maghanoy <kierveymaghanoy24@gmail.com>
2023-01-13 01:54:51 +0800

    add post resource

---
+++ app/Http/Resources/Post/PostCollection.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Resources\Post;
+
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class PostCollection extends ResourceCollection
+{
+    /**
+     * Transform the resource collection into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'data' => $this->collection->transform(function($request) {
+                return [
+                    'id' => $request->id,
+                    'description' => $request->description,
+                    'photo' => $request->photo,
+                    'total_votes' => $request->total_votes
+                ];
+            }),
+            'meta' => [
+                'total' => $this->total(),
+                'page' => $this->currentPage(),
+                'perPage' => (int) $this->perPage(),
+                'totalPages' => $this->lastPage(),
+                'path' => $this->path()
+            ]
+        ];
+    }
+}

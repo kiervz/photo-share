475d9757e912dd9149a40924de4e2161793aaf5b
Kiervey Maghanoy <kierveymaghanoy24@gmail.com>
2023-01-12 23:26:00 +0800

    create models and migrations

---
+++ database/migrations/2023_01_12_151555_create_votes_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use App\Models\Post;
+use App\Models\User;
+
+class CreateVotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('votes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Post::class, 'post_id');
+            $table->foreignIdFor(User::class, 'user_id');
+            $table->smallInteger('vote');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('votes');
+    }
+}
